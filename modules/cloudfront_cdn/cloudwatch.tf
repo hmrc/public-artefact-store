2ed1e999eb9470d3fd11ad972869b4af42720109
cob16 <public+github@cormacbrady.info>
2021-04-27 11:39:39 +0100

    run Terraform fmt
    Co-authored-by: Masquerainsley <ainsley.gilligan@digital.hmrc.gov.uk>

--- modules/cloudfront_cdn/cloudwatch.tf
+++ modules/cloudfront_cdn/cloudwatch.tf
@@ -2,63 +2,63 @@ resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = local.aws_resource_safe_domain_name
 
   dashboard_body = jsonencode({
-    "widgets": [
-        {
-            "height": 6,
-            "width": 9,
-            "y": 0,
-            "x": 0,
-            "type": "log",
-            "properties": {
-                "query": "SOURCE '${module.cloudfront-logs.logs_cloudwatch_log_group.name}' | ${aws_cloudwatch_query_definition.request_status_codes.query_string}",
-                "region": "eu-west-2",
-                "stacked": false,
-                "view": "pie",
-                "title": "Request Status Codes"
-            }
-        },
-        {
-            "height": 6,
-            "width": 9,
-            "y": 0,
-            "x": 9,
-            "type": "log",
-            "properties": {
-                "query": "SOURCE '${module.cloudfront-logs.logs_cloudwatch_log_group.name}' | ${aws_cloudwatch_query_definition.requests_by_edge_location.query_string}",
-                "region": "eu-west-2",
-                "stacked": false,
-                "view": "pie",
-                "title": "Requests by Edge Location"
-            }
-        },
-        {
-            "height": 9,
-            "width": 18,
-            "y": 6,
-            "x": 0,
-            "type": "log",
-            "properties": {
-                "query": "SOURCE '${module.cloudfront-logs.logs_cloudwatch_log_group.name}' | ${aws_cloudwatch_query_definition.top_requested_files.query_string}",
-                "region": "eu-west-2",
-                "stacked": false,
-                "view": "bar",
-                "title": "Top 10 File Requests"
-            }
-        },
-        {
-            "height": 15,
-            "width": 6,
-            "y": 0,
-            "x": 18,
-            "type": "log",
-            "properties": {
-                "query": "SOURCE '${module.cloudfront-logs.logs_cloudwatch_log_group.name}' | ${aws_cloudwatch_query_definition.request_count_by_ip.query_string}",
-                "region": "eu-west-2",
-                "stacked": false,
-                "view": "table",
-                "title": "Request count by IP"
-            }
+    "widgets" : [
+      {
+        height : 6,
+        width : 9,
+        y : 0,
+        x : 0,
+        type : "log",
+        properties : {
+          "query" : "SOURCE '${module.cloudfront-logs.logs_cloudwatch_log_group.name}' | ${aws_cloudwatch_query_definition.request_status_codes.query_string}",
+          "region" : "eu-west-2",
+          "stacked" : false,
+          "view" : "pie",
+          "title" : "Request Status Codes"
         }
+      },
+      {
+        height : 6,
+        width : 9,
+        y : 0,
+        x : 9,
+        type : "log",
+        properties : {
+          "query" : "SOURCE '${module.cloudfront-logs.logs_cloudwatch_log_group.name}' | ${aws_cloudwatch_query_definition.requests_by_edge_location.query_string}",
+          "region" : "eu-west-2",
+          "stacked" : false,
+          "view" : "pie",
+          "title" : "Requests by Edge Location"
+        }
+      },
+      {
+        height : 9,
+        width : 18,
+        y : 6,
+        x : 0,
+        type : "log",
+        properties : {
+          "query" : "SOURCE '${module.cloudfront-logs.logs_cloudwatch_log_group.name}' | ${aws_cloudwatch_query_definition.top_requested_files.query_string}",
+          "region" : "eu-west-2",
+          "stacked" : false,
+          "view" : "bar",
+          "title" : "Top 10 File Requests"
+        }
+      },
+      {
+        height : 15,
+        width : 6,
+        y : 0,
+        x : 18,
+        type : "log",
+        properties : {
+          "query" : "SOURCE '${module.cloudfront-logs.logs_cloudwatch_log_group.name}' | ${aws_cloudwatch_query_definition.request_count_by_ip.query_string}",
+          "region" : "eu-west-2",
+          "stacked" : false,
+          "view" : "table",
+          "title" : "Request count by IP"
+        }
+      }
     ]
   })
 }
