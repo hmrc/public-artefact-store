a91ec884b951634a6658f9218bb92c8c72d9a3e3
cob16 <public+github@cormacbrady.info>
2021-04-30 15:19:10 +0100

    add GeneralRateLimit

--- modules/cloudfront_waf/waf.tf
+++ modules/cloudfront_waf/waf.tf
@@ -1,3 +1,5 @@
+# please note as this is a public repo please only add rules that you wish to be known i.e rate-limit
+
 resource "aws_wafv2_web_acl" "waf_acl" {
   provider = aws.us_east_1
 
@@ -15,4 +17,26 @@ resource "aws_wafv2_web_acl" "waf_acl" {
     metric_name                = var.name_prefix
     sampled_requests_enabled   = true
   }
+
+  rule {
+    name     = "GeneralRateLimit"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = (100 * 60 * 5) # 100 requests per second in a 5-minute time span
+        aggregate_key_type = "IP"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      sampled_requests_enabled   = false
+      metric_name                = "GeneralRateLimit"
+    }
+  }
 }
+
