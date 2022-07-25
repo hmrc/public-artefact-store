70ffe773b614cf64ba2c696f54cc01c81251bbbf
Lewis Capaldi <55383463+fruiti-lewis@users.noreply.github.com>
2022-07-25 12:10:23 +0100

    BAU Add roles in all perms

--- modules/s3_bucket/modules.tf
+++ modules/s3_bucket/modules.tf
@@ -4,13 +4,28 @@ module "bucket" {
   bucket_name   = var.bucket_name
   force_destroy = false
 
-  list_roles = [local.build_and_deploy_api_lambda_role]
+  list_roles = [
+    aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn,
+    local.build_and_deploy_api_lambda_role,
+    "arn:aws:iam::893174414079:role/RoleBuildEngineer",
+    "arn:aws:iam::893174414079:role/MDTPBuildCodeBuild",
+    "arn:aws:iam::565237821078:role/RoleBuildEngineer",
+    "arn:aws:iam::565237821078:role/MDTPBuildCodeBuild",
+  ]
   read_roles = [
     aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn,
-    local.build_and_deploy_api_lambda_role
+    local.build_and_deploy_api_lambda_role,
+    "arn:aws:iam::893174414079:role/RoleBuildEngineer",
+    "arn:aws:iam::893174414079:role/MDTPBuildCodeBuild",
+    "arn:aws:iam::565237821078:role/RoleBuildEngineer",
+    "arn:aws:iam::565237821078:role/MDTPBuildCodeBuild",
   ]
   write_roles = [
-    local.build_and_deploy_api_lambda_role
+    local.build_and_deploy_api_lambda_role,
+    "arn:aws:iam::893174414079:role/RoleBuildEngineer",
+    "arn:aws:iam::893174414079:role/MDTPBuildCodeBuild",
+    "arn:aws:iam::565237821078:role/RoleBuildEngineer",
+    "arn:aws:iam::565237821078:role/MDTPBuildCodeBuild",
   ]
   admin_roles = [
     "arn:aws:iam::893174414079:role/RoleBuildEngineer",
