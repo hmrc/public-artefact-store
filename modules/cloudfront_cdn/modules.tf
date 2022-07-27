fb070673f8dcdb428635d3ca68df0829fbd3e6cb
Aadam Ali <57071686+Aadam-Ali@users.noreply.github.com>
2022-07-27 16:30:05 +0100

    PBD-3746 Fixed CloudFront logging with custom KMS key
    Co-authored-by: Neil Millard <3050649+neilmillard@users.noreply.github.com>

--- modules/cloudfront_cdn/modules.tf
+++ modules/cloudfront_cdn/modules.tf
@@ -6,3 +6,57 @@ module "cloudfront-logs" {
   retention   = 365
   tags        = var.tags
 }
+
+resource "aws_kms_key" "cloudfront_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+  policy                  = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "key-default-1",
+    "Statement": [
+        {
+            "Sid": "Enable IAM User Permissions",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+            },
+            "Action": "kms:*",
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow CloudFront to use the key to deliver logs",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "delivery.logs.amazonaws.com"
+            },
+            "Action": [
+                "kms:GenerateDataKey*",
+                "kms:Decrypt"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "kmsdecrypt",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${module.cloudfront-logs.this_lambda_function.role}"
+            },
+            "Action": "kms:Decrypt",
+            "Resource": "*"
+        }
+    ]
+  }
+  EOF
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = module.cloudfront-logs.this_s3_bucket.bucket
+  rule {
+    bucket_key_enabled = true
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.cloudfront_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
